1d5df85594452951ae7cb285dc186171d802b3d3
gcastill0-s1 <179959151+gcastill0-s1@users.noreply.github.com>
2024-09-06 09:50:04 -0400

    Adding HTTP_RESPONSE full record capture

---
+++ project-01/case-01/tscript-02.js
@@ -0,0 +1,19 @@
+// s1_ODS_target is configured as a target for an Open Data Stream
+const s1_path = "/services/collector/raw?sourcetype=extrahop"
+
+// Check if the event is HTTP_REQUEST or HTTP_RESPONSE to capture transaction data
+
+if (HTTP) {
+    // Create a JSON object with full record
+    var jsonData = HTTP.record;
+
+    // Convert the JSON object to a string
+    var jsonString = JSON.stringify(jsonData);
+
+    // Send the JSON data to the webhook URL using an HTTP POST request
+    Remote.HTTP('s1_ODS_target').post({
+        'path' : s1_path,
+        'payload' : jsonString
+    });
+
+}
